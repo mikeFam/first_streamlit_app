5d93791360f98d0b6c0f5f0f896cadae09726394
mikeFam <33562132+mikeFam@users.noreply.github.com>
2022-04-19 09:53:08 +1200

    Remove debugging statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,20 +28,20 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-streamlit.text("code works fine before import")
+
 import snowflake.connector
 
-streamlit.text("hello mofo!")
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
-streamlit.text("got a connection")
+
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-streamlit.text("the cursor execute something!")
+
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+
 streamlit.text(my_data_row)
-streamlit.text("it fetched something")
+
 
 
