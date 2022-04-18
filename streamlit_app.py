6409d08bf6448257740b00f7e0c6d818c0719902
mikeFam <33562132+mikeFam@users.noreply.github.com>
2022-04-18 16:16:30 +1200

    Look inside response

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,5 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
