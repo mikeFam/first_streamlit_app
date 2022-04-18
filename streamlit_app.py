acc759c47004d051c1328f5c25fe89fbbeefa8fd
mikeFam <33562132+mikeFam@users.noreply.github.com>
2022-04-18 16:15:47 +1200

    Call fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas 
+import requests
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Favorites')
@@ -17,4 +18,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+
+#New Section to display fruityvice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
