36a492ab20ca8e2a54522aa2be9bb85200212d88
mikeFam <33562132+mikeFam@users.noreply.github.com>
2022-04-19 10:13:56 +1200

    rew

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,7 @@ my_data = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data)
 
-
+add_my_fruit = streamlit.text_input('what fruit wowuld like to add?')
+if add_my_fruit is not none:
+  streamlit.text(f'thank you for adding {add_my_fruit}')
 
